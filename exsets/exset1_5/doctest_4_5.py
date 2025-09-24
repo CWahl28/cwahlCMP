f60bc8af44a9e6fe181b2d55fbc82fd53236873b
CWahl28 <Claranwahl@gmail.com>
2025-09-24 09:35:12 -0400

    forgor

---
+++ exsets/exset1_5/doctest_4_5.py
@@ -0,0 +1,19 @@
+def is_even(n):
+    """
+        >>> is_even(2)
+        "True"
+        >>> is_even(5)
+        "False"
+        >>> is_even(18)
+        "True"
+    """
+
+if n % 0:
+    print("True")
+else:
+    print("False")
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
